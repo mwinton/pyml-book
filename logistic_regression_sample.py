61fb636e4b3b4acc8cea1c3c32e5086c539d7cb2
mwinton <winton@google.com>
2017-10-19 12:28:33 -0700

    Add files via upload

--- logistic_regression_sample.py
+++ logistic_regression_sample.py
@@ -93,10 +93,11 @@ class LogisticRegressionGD(object):
       Sum of squares cost function value in each epoch.
     """
     
-    def __init__(self, eta=0.05, n_iter=100, random_state=1):
+    def __init__(self, eta=0.05, n_iter=100, random_state=1, to_normalize=False):
         self.eta = eta
         self.n_iter = n_iter
         self.random_state = random_state
+        self.to_normalize = to_normalize
 
     def fit(self, X, y):
         """Fit training data.
@@ -112,13 +113,17 @@ class LogisticRegressionGD(object):
         -------
         self : object
         """
- 
-#        X = self.normalize(X)
+        
+#       NORMALIZATION ISN'T WORKING YET 
+        if self.to_normalize:
+            X = self.normalize(X)
         
         rgen = np.random.RandomState(self.random_state)
         self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
         self.cost_ = []
 
+        #NOTE: Need to switch to gradient descent, rather than blindly doing n_iter iterations
+        #NOTE: Need to switch from eta to 1/m 
         for i in range(self.n_iter):
             net_input = self.net_input(X)
             output = self.sigmoid(net_input)
@@ -130,7 +135,7 @@ class LogisticRegressionGD(object):
         return self
 
     def net_input(self, X):
-        """Calculate net input"""
+        """Calculate net input.  This term is z in Andrew Ng's class """
         return np.dot(X, self.w_[1:]) + self.w_[0]
 
     def sigmoid (self, z):
@@ -142,7 +147,7 @@ class LogisticRegressionGD(object):
         '''Calculate the logistic regression cost function'''
         # NOTE: need to include the 1/m term
         # NOTE: need to update to return both J and Gradient
-        return (-y.dot(np.log(output)) - ((1-y).dot(np.log(1-output))))
+        return (-y.dot(np.log(output)) - ((1.-y).dot(np.log(1.-output))))
         
     def gradient_descent (self, X, y, w):
         '''Performs gradient descent to optimize'''
@@ -153,15 +158,36 @@ class LogisticRegressionGD(object):
         '''Normalizes feature vectors'''
         
         X_norm = np.copy(X)
-        # NOTE: need to generalize this to work for any number of columns
-        # NOTE: also need to store normalization factors (for recovery)
-        X_norm[:,0] = (X[:,0] - X[:0].mean()) / X[:,0].std()
-        X_norm[:,1] = (X[:,1] - X[:1].mean()) / X[:,1].std()
+        num_cols = X_norm.shape[1]
+        self.X_means = [] # list will have one entry for each feature column of X
+        self.X_stds = [] # list will have one entry for each feature column of X
+        
+        for i in range(0,num_cols):
+            X_norm[:,i] = (X[:,i] - X[:,i].mean()) / X[:,i].std()
+            # To normalize by range (instead of sigma) use the following line:
+            #X_norm[:,i] = (X[:,i] - X[:,i].mean()) / (X[:,i].max()-X[:,i].min())
+
+            #Store normalization factors to restore predicted values later
+            self.X_means.append(X[:,i].mean())
+            self.X_stds.append(X[:,i].std())
+            # To normalize by range (instead of sigma) use the following line:
+            #self.X_stds.append((X[:,i].max()-X[:,i].min()))
+        print('During normalization, calculated mean=',self.X_means,'std=',self.X_stds)
         return X_norm
         
     def predict(self, X):
         """Return class label based on sigmoid function"""
-        return np.where(self.sigmoid(self.net_input(X)) >= 0.5, 1, 0)
+        
+        if self.to_normalize:
+            X_norm = np.copy(X)
+            for i in range(0,X_norm.shape[1]):
+                # During prediction, need to use the saved means/stds, rather than recalculate
+                X_norm[:,i] = (X[:,i] - self.X_means[i]) / self.X_stds[i]
+            return np.where(self.net_input(X_norm) >= 0, 1, 0)
+        else:
+            #Due to shape of sigmoid function, these two options are equivalent
+            return np.where(self.net_input(X) >= 0, 1, 0)
+#           return np.where(self.sigmoid(self.net_input(X)) >= 0.5, 1, 0)
 
 
 
@@ -208,16 +234,26 @@ def prepareDataset(model = 'lr'):
     # select setosa and verginica (using only 100 of the 150 samples for training)
     y = df.iloc[0:100, 4].values
     
-    # modify y so that setosa --> -1 / 0 and everything else --> 1
+    # deal with the fact that perceptron treats negative cases as -1; LR treats as 0
+    negval = 0
+    posval = 1
     if model == 'p':
-        y = np.where(y == 'Iris-setosa', -1,1)
-    else:
-        y = np.where(y == 'Iris-setosa', 0,1)
+        negval = -1
+
+    # modify y so that setosa --> -1 / 0 and everything else --> 1
+    y = np.where(y == 'Iris-setosa', negval,posval)
         
     # extract sepal length and petal length (cols 0, 2)
     X = df.iloc[0:100, [0,2]].values
 
     # plot data to make sure it's linearly separable (otherwise perceptron won't work)
+
+#   NOTE: THIS CODE IS NOT WORKING YET because X is an numpy Array, not a List.
+#    series_neg = [val for is_good, val in zip(y,X) if is_good == negval]
+#    series_pos = [val for is_good, val in zip(y,X) if is_good == posval]
+#    plt.scatter(series_neg[,0], series_neg[,1], color='red', marker='o', label='Setosa')
+#    plt.scatter(series_pos[:,0], series_pos[:,1], color='blue', marker = 'x', label='Virginica')
+
     plt.scatter(X[:50,0], X[:50,1], color='red', marker='o', label='Setosa')
     plt.scatter(X[50:100,0], X[50:100,1], color='blue', marker = 'x', label='Virginica')
     plt.xlabel('sepal length [cm]')
@@ -250,33 +286,27 @@ def runPerceptronModel():
       
 def runLRModel():
     # Train logistic regression classifier
+    norm = True
     X,y = prepareDataset(model = 'lr')
-    lrgd = LogisticRegressionGD(eta=0.05, n_iter = 10000, random_state = 1)
+    lrgd = LogisticRegressionGD(eta=0.05, n_iter = 10000, random_state = 1, to_normalize = norm)
     lrgd.fit(X,y)
 
     plot_decision_regions(X,y, classifier=lrgd)
     plt.xlabel('sepal length [cm]')
     plt.ylabel('petal length [cm]')
-    plt.title('Iris classification (logistic regression)')
+    plt.title('Iris classification (logistic regression); Normalized= '+str(norm))
     plt.legend()
     plt.show()
     
     # Run a few test cases through
-    print(lrgd.predict([3,5])) # should be 1
-    print(lrgd.predict([7,2])) # should be 0
-    print(lrgd.predict([5,3])) # should be 1
-    print(lrgd.predict([7,4])) # should be 1
-    print(lrgd.predict([6,1])) # should be 0
+    print('Running a few test cases...')
+    print(lrgd.predict(np.array([[3.,5.]])),'(should be 1)') # should be 1
+    print(lrgd.predict(np.array([[7.,1.]])),'(should be 0)') # should be 0
+    print(lrgd.predict(np.array([[5.,3.]])),'(should be 1)') # should be 1
+    print(lrgd.predict(np.array([[7.,4.]])),'(should be 1)') # should be 1
+    print(lrgd.predict(np.array([[6.,1.]])),'(should be 0)') # should be 0
     
    
-def testLRModel():
-    ''' 
-    Run a few test cases through.
-    '''
-    pass
-
 
 #runPerceptronModel()
-
 runLRModel()
-#testLRModel()
